b109138b70d8bbdfa9c7cb4787a08eeb14419fac
HungggAnhh <vmha0312@gmail.com>
2026-04-15 15:35:58 +0700

    fix vercel

--- styles/scripts/setup-styles.ts
+++ styles/scripts/setup-styles.ts
@@ -1,4 +1,4 @@
-import * as config from '../config'
+import * as config from '../config.ts'
 import { generateRoot } from './generate-root'
 import { generateScale } from './generate-scale'
 import { generateTailwind } from './generate-tailwind'
